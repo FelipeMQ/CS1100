984cf8ba450aa5364fe97f45988c42bffffe001b
randiel.melgarejo <randiel.melgarejo@gmc-soft.com>
2017-06-05 11:22:31 -0500

    Definiciones de diccionarios

---
+++ week11/diccionarios0.py
@@ -0,0 +1,10 @@
+cosas = {'nombre':'Prof. CS1100',
+         'edad':39,
+         'altura': 1.76 }
+print(cosas)
+print(cosas['nombre'])
+print(cosas['edad'])
+print(cosas['altura'])
+
+for value in cosas.items():
+    print(value)
