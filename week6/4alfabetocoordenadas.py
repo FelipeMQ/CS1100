c84300e9768291b08a2825ad092e4119bf9e44dd
Randiel Melgarejo <randielmelgarejo@gmail.com>
2017-10-16 08:42:43 -0500

    Ejemplo del alfabeto en coordenadas

--- week6/4alfabetocoordenadas.py
+++ week6/4alfabetocoordenadas.py
@@ -1,7 +1,11 @@
 frase = input()
-letras = []
-
-
-if not frase == frase.upper():
+letras = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','Ñ','O','P','Q','R','S','T','U','V','W','Y','X','Z','¿','?','-','0','1','2','3','4','5','6','7','8','9','¡','!']
+_colum = [0,1,2,3,4,5,0,1,2,3,4,5,0,1,2,3,4,5,0,1,2,3,4,5,0,1,2,3,4,5,0,1,2,3,4,5,0,1,2,3,4,5]
+_filas = [0,0,0,0,0,0,1,1,1,1,1,1,2,2,2,2,2,2,3,3,3,3,3,3,4,4,4,4,4,4,5,5,5,5,5,5,6,6,6,6,6,6]
+if frase == frase.upper():
+    for c in range(len(frase)):
+        if (frase[c] in letras and (not frase[c]==' ')):
+            posicion = letras.index(frase[c])
+            print(_filas[posicion], _colum[posicion])
+else:
     print("Valor incorrecto")
-
